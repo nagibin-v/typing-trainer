616b42aac903bbcc747cff35da98baacaf92a33f
Vsevolod Nagibin <vsevolod.nagibin@ya.ru>
2023-04-07 19:34:59 +0300

    Wrote run.sh and init.sh

---
+++ init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+virtualenv -p python3 venv
+python -m pip install requirements.txt
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PySide6 import QtWidgets as qtw, QtCore as qtc
+from PySide6 import QtWidgets as qtw
 from __feature__ import snake_case, true_property
 import sys
 from main_window import MainWindow
